ca06fdc734f92e1630aac1f3014c34335b2263d4
HoCheon95 <dcr584@gmail.com>
2026-01-13 19:31:57 +0900

    new work

--- Day18_java/src/중첩클래스와중첩인터페이스/Anonymous.java
+++ Day18_java/src/중첩클래스와중첩인터페이스/Anonymous.java
@@ -8,7 +8,39 @@ public class Anonymous {
 	
 	//필드 초기값으로 대입
 	Person field = new Person() {
+		void work() {
+			System.out.println("출근을 합니다.");
+		}
+
+		@Override
+		void wake() {
+			System.out.println("6시에 일어납니다.");
+			work();
+		}
 		
-	};//첫번쨰 익명클래스(외부클래스명$번호.class - Anonymous.$1.class)
+		
+	};//첫번쨰 익명클래스(외부클래스명$번호.class -> Anonymous.$1.class)
+	
+	void method() {
+		// 지역변수값으로 대입
+		Person localVar = new Person() {
+			void walk() {
+				System.out.println("산책을 합니다.");
+			}
+
+			@Override
+			void wake() {
+				System.out.println("7시에 일어납니다.");
+				walk();
+			}
+			
+		};//두번째 익명 클래스(외부클래스명$번호.class -> Anonymous.$2.class)
+		
+		localVar.wake();
+	}//method()
+	
+	void method02(Person person) {//매개변수 인자값으로 익명객체 대입
+		person.wake();
+	}
 
 }
