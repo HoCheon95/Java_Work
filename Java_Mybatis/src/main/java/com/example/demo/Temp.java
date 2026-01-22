567cba294559fb340b525094180d15d9f377ccf9
HoCheonNoteBook95 <dcr584@gmail.com>
2026-01-22 14:56:42 +0900

    한글 입력 에러

---
+++ Java_Mybatis/src/main/java/com/example/demo/Temp.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import java.util.Scanner;
+
+public class Temp {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in, "UTF-8");
+        String qwe = scan.nextLine();
+        System.out.println(qwe);
+    }
+}
--- Java_Mybatis/src/test/java/com/example/demo/SystemInfo.java
+++ Java_Mybatis/src/test/java/com/example/demo/SystemInfo.java
@@ -1,10 +1,11 @@
 package com.example.demo;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class SystemInfo {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in, StandardCharsets.UTF_8);
 
         System.out.print("한글 이름을 입력하세요 : ");
         String name = scan.nextLine();
